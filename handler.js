e2048bcb6eba8d2ec125051b0a5cbad6ed420992
Preston Wallace <wallace.preston@gmail.com>
2022-10-25 07:21:40 -0700

    add ses email support

--- handler.js
+++ handler.js
@@ -1,11 +1,31 @@
 'use strict';
+const AWS = require('aws-sdk');
+const ses = new AWS.SES({ region: 'us-west-2' });
 
 module.exports.hello = async (event) => {
+  const queryParams = event.queryStringParameters || {};
+  const params = {
+    Destination: {
+      ToAddresses: [queryParams.email],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: queryParams.message,
+        },
+      },
+      Subject: {
+        Data: queryParams.subject,
+      },
+    },
+    Source: 'example@example.com', // This can be any email address, the email you want to show as the "sender" when the email is received
+  };
+  await ses.sendEmail(params).promise();
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: `Email sent to ${queryParams.email}`,
         input: event,
       },
       null,
