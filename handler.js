cde6a602bb3b4f37156758fefe496d67ad9b08a1
Preston Wallace <wallace.preston@gmail.com>
2022-10-25 15:17:05 -0700

    update ses params to use dynamic values

--- handler.js
+++ handler.js
@@ -5,28 +5,41 @@ const ses = new AWS.SES({ region: 'us-west-2' });
 module.exports.hello = async (event) => {
   try {
     const queryParams = event.queryStringParameters || {};
+
+    // select from the query parameters or use the default values
+    const {
+      email = 'example@example.com',
+      message = 'This message is from the Notification Service',
+      subject = 'Message from the Notification Service',
+    } = queryParams;
+
+    // build params object to pass to the sendEmail function
     const params = {
       Destination: {
-        ToAddresses: ['wallace.preston@gmail.com'], // the email address you want to send to
+        ToAddresses: [email],
       },
       Message: {
         Body: {
           Text: {
-            Data: 'This is a test email.',
+            Data: message,
           },
         },
         Subject: {
-          Data: 'Test Email',
+          Data: subject,
         },
       },
       Source: 'wallace.preston@gmail.com', // The email you want to show as the "sender" when the email is received.  This must be added as an identity via the AWS SES console and verified.
     };
+
+    // send the email
     await ses.sendEmail(params).promise();
+
+    // response to the client that made the request
     return {
       statusCode: 200,
       body: JSON.stringify(
         {
-          message: `Email sent to example@example.com`,
+          message: `Email sent to ${email}`,
           input: event,
         },
         null,
@@ -35,9 +48,5 @@ module.exports.hello = async (event) => {
     };
   } catch (error) {
     console.log(error);
-    return error;
   }
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
