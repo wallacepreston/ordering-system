525eb45658bbb6bfb76b59ec744778945a0c535b
Preston Wallace <wallace.preston@gmail.com>
2022-10-25 07:31:13 -0700

    debug code

--- handler.js
+++ handler.js
@@ -3,35 +3,41 @@ const AWS = require('aws-sdk');
 const ses = new AWS.SES({ region: 'us-west-2' });
 
 module.exports.hello = async (event) => {
-  const queryParams = event.queryStringParameters || {};
-  const params = {
-    Destination: {
-      ToAddresses: [queryParams.email],
-    },
-    Message: {
-      Body: {
-        Text: {
-          Data: queryParams.message,
+  try {
+    
+    const queryParams = event.queryStringParameters || {};
+    const params = {
+      Destination: {
+        ToAddresses: ['example@example.com'],
+      },
+      Message: {
+        Body: {
+          Text: {
+            Data: 'this is a test email',
+          },
+        },
+        Subject: {
+          Data: 'Test Email',
         },
       },
-      Subject: {
-        Data: queryParams.subject,
-      },
-    },
-    Source: 'example@example.com', // This can be any email address, the email you want to show as the "sender" when the email is received
-  };
-  await ses.sendEmail(params).promise();
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: `Email sent to ${queryParams.email}`,
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+      Source: 'example@example.com', // This can be any email address, the email you want to show as the "sender" when the email is received
+    };
+    await ses.sendEmail(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: `Email sent to example@example.com`,
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
